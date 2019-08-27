f618fcb4c3d6c1d2e4f1c1834da123b277e0b042
ljc43026 <ljc43026@ly.com>
2019-08-27 15:14:25 +0800

    hihttp first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# hihttp
+
+hihttp 是 golang http 库客户端的简单封装，实现了一些方便开发的常用功能。
+
+## 功能：
+
+- **参数按添加顺序发送**
+- **transport 连接复用**
+- **head 不自动规范化**
+- **代理（http、https、socks5）**
+  1. 设置默认代理
+  2. 设置不同 url 对应的代理
+- **单个请求的超时时间**
+- **重定向检查**
+- **添加 cookie**
+- **支持 context 传入**
+- **post 多种数据类型**
+- **json 结果解析为结构体**
+- **结果存入文件**
+
+## TODO:
+
+- 重试
+- 支持 debug 模式 打印请求和结果信息
+- 文件上传
+- 代理选择器 每个请求轮换/每多少次轮换
+- cookiejar支持启用PublicSuffixList
+- golang.org/x/net/proxy代理设置
---
+++ example_test.go
@@ -0,0 +1,62 @@
+package hihttp
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+)
+
+func ExampleGet() {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello,hihttp"))
+	}))
+	defer srv.Close()
+
+	code, data, err := Get(srv.URL)
+	fmt.Println(code, string(data), err)
+
+	// Output:
+	// 200 hello,hihttp <nil>
+}
+
+func ExamplePost() {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hi", r.FormValue("hi"))
+		fmt.Println("go", r.FormValue("go"))
+		fmt.Println("http", r.FormValue("http"))
+	}))
+	defer srv.Close()
+
+	postData := url.Values{}
+	postData.Add("hi", "everyone")
+	postData.Add("go", "is a good language")
+	postData.Add("http", "is a good package")
+	code, data, err := Post(srv.URL, postData.Encode(), PostOption{ContentType: CTApplicationForm})
+	fmt.Println(code, string(data), err)
+
+	// Output:
+	// hi everyone
+	// go is a good language
+	// http is a good package
+	// 200  <nil>
+}
+
+func ExampleProxy() {
+	SetProxy("socks5://127.0.0.1:1080")
+	// SetProxy("http://127.0.0.1:1080")
+	// SetProxy("https://127.0.0.1:1080")
+	// SetAuthProxy("socks5://127.0.0.1:1080", "proxyUserName", "proxyPassword")
+	// SetAuthProxy("http://127.0.0.1:1080", "proxyUserName", "proxyPassword")
+	// SetAuthProxy("https://127.0.0.1:1080", "proxyUserName", "proxyPassword")
+
+	statusCode, _, err := Get("https://www.google.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(statusCode, err)
+
+	// Output:
+	// 200  <nil>
+}
